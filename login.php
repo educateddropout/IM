c5ef88480c0a5397ff72f4bbefef3fabaf152e00
EducatedDropout <kenanthonymolina@gmail.com>
2017-10-11 03:12:12 +0800

    Initial as of 10/11/2017

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<title>Registration/Login Page</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="lib/css/v4/w3.css">
+<link rel="stylesheet" href="lib/css/font-awesome-4.7.0/css/font-awesome.min.css">
+<script src="lib/js/angular.min.js"></script>
+<script type="text/javascript" src="Angular/login.js"></script>
+<body class="w3-hover-light-grey" ng-app="login">
+	<!-- Navbar (sit on top) -->
+	<div class="w3-top">
+	  <div class="w3-white w3-wide w3-padding w3-card-2 w3-hover-white">
+	  	<div class="w3-center">
+	    	<a href="#home" class="w3-bar-item w3-button"><b>IM</b>LifeStyle</a>
+	    </div>
+	  </div>
+	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="w3-container">
+		<div class="w3-row">
+			<div class="w3-col m3">&nbsp</div>
+			<div class="w3-col m6 w3-white">
+				<div class="w3-row w3-border w3-text-grey">
+					<div class="w3-row w3-center">
+						<a href="javascript:void(0)">
+							<div class="w3-half tablink w3-border-bottom w3-border-blue w3-border-right w3-hover-light-grey w3-padding"><b>SIGN UP</b></div>
+						</a>
+						<a href="javascript:void(0)">
+							<div class="w3-half tablink w3-border-bottom w3-border-blue w3-hover-light-grey w3-padding"><b>SIGN IN</b></div>
+						</a>
+					</div>
+
+					<!-- <sign-up></sign-up> -->
+
+					<!-- Sign In Division -->
+					<sign-in></sign-in>
+
+				</div>
+			</div>
+			<div class="w3-col m3">&nbsp</div>
+		</div>
+		<br>
+		<br>
+	</div>
+</body>
+</html>
